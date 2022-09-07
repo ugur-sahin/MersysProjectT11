4b886abc87c3eb11c0aef17084f18dbaadbf8516
uurshn <ugursahin260@gmail.com>
2022-09-07 17:57:50 +0300

    deneme 07092022

--- src/test/java/Pages/deneme.java
+++
@@ -1,7 +0,0 @@
-package Pages;
-
-public class deneme {
-    public static void main(String[] args) {
-        System.out.println("Merhaba");
-    }
-}
--- src/test/java/Pages/yenidenme.java
+++
@@ -1,7 +0,0 @@
-package Pages;
-
-public class yenidenme {
-    public static void main(String[] args) {
-        System.out.println("ghbnjvmnbö");
-    }
-}
