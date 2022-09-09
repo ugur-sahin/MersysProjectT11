8e553d0af4ce10ebe38a6b2cdc003e4d4b2aae24
arc.mac <selimqatester@gmail.com>
2022-09-09 21:39:22 +0300

    Test11_03_v2

--- src/test/java/StepDefinitions/TEST11_03_DocumentTypes.java
+++ src/test/java/StepDefinitions/TEST11_03_DocumentTypes.java
@@ -3,5 +3,6 @@ package StepDefinitions;
 public class TEST11_03_DocumentTypes {
     public static void main(String[] args) {
         System.out.println("merhaba");
+        System.out.println("merhaba");
     }
 }
