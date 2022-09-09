e0f9cd1b66b2bdf6c8a30abf11a81f494bddabfa
reyyan35 <reyyanyilmaz@gmail.com>
2022-09-09 19:43:15 +0300

    d2

--- src/test/java/StepDefinitions/TEST11_08_.java
+++ src/test/java/StepDefinitions/TEST11_08_.java
@@ -3,5 +3,6 @@ package StepDefinitions;
 public class TEST11_08_ {
     public static void main(String[] args) {
         System.out.println("merhaba");
+        System.out.println("merhaba 2");
     }
 }
